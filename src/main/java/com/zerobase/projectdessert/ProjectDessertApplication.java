99bd41958c6852d5f6a7ce4956099216771e7183
hjuuujh <hjuuujh@gmail.com>
2024-08-29 20:07:24 +0900

    project base

---
+++ src/main/java/com/zerobase/projectdessert/ProjectDessertApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.projectdessert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDessertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDessertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zerobase/projectdessert/ProjectDessertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.projectdessert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDessertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
