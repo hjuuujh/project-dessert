62a8c5d8851207d0ed02e4d6c25592f450a4cdde
hjuuujh <hjuuujh@gmail.com>
2024-09-11 20:52:55 +0900

    feat: 012-docker-compose
    - docker-compose

---
+++ init/init.sql
@@ -0,0 +1,6 @@
+create database `member`;
+GRANT ALL ON `member`.* TO 'root'@'%';
+create database `store`;
+GRANT ALL ON `store`.* TO 'root'@'%';
+create database `orders`;
+GRANT ALL ON `orders`.* TO 'root'@'%';
